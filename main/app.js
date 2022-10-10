a996c1604f0a22e1105f939e95133e0db4d6363a
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-10 23:26:50 +0600

    try to catch error

---
+++ main/app.js
@@ -0,0 +1,17 @@
+const result = document.getElementById("result");
+const submit = document.getElementById("submit");
+
+function binary(e){
+e.preventDefault();
+
+
+const num = document.getElementById("number").value;
+if(num === ''){
+    alert('Please enter a number')
+}
+else if(num < 0){
+    alert('Please enter a positive number')
+}
+}
+
+submit.addEventListener('click',binary);
